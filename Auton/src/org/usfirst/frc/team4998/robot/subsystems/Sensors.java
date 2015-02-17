65d3d2a3159b8835563bd9f6bda97f82d165c981
andrew <anwhitle@umflint.edu>
2015-02-17 18:01:03 -0500

    Sensors added

--- Auton/src/org/usfirst/frc/team4998/robot/subsystems/Sensors.java
+++ Auton/src/org/usfirst/frc/team4998/robot/subsystems/Sensors.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4998.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -7,21 +8,35 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Sensors extends Subsystem {
-	Ultrasonic ultrasonic1 = new Ultrasonic(1 , 2);
-	Ultrasonic ultrasonic2 = new Ultrasonic(3 , 4);
-	
-    
+ Ultrasonic ultrasonic1 = new Ultrasonic(1 , 2);
+ Ultrasonic ultrasonic2 = new Ultrasonic(3 , 4);
+ Encoder encoder1 = new Encoder(1 , 2);
+ Encoder encoder2 = new Encoder(3 , 4);
+    Double x, y;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
+    
+     x = getX();
+     y = getY();
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    public void getX(){
-    	ultrasonic1.getDistanceUnits();
+    public Double getX(){
+     return ultrasonic1.getRangeMM();
     }
     
+    public Double getY(){
+     return ultrasonic2.getRangeMM();
+    }
+    
+    public void updateX(){
+     x = x + encoder1.getDistance();
+    }
+    
+    public void updateY(){
+     y = y + encoder2.getDistance();
+    }
 }
-
